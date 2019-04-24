8a734ad7c5ee95d22a5da66ce3f06efeb3895b42
Pranawa Somvedi <pranawab@gmail.com>
2019-04-24 11:54:43 +0100

    inheritance

--- customer.py
+++ customer.py
@@ -1,20 +1,9 @@
 class customer(object):
 
     def __init__(self, id, gender, nationality, firstName, lastName, streetAddress, zipCode, city, emailAddress, username, phoneNumber):
-        self.id = id
-        self.gender = gender
-        self.nationality = nationality
-        self.firstName = firstName
-        self.lastName = lastName
-        self.streetAddress = streetAddress
-        self.zipCode = zipCode
-        self.city = city
-        self.emailAddress = emailAddress
-        self.username = username
-        self.phoneNumber = phoneNumber
+        super().__init__(id, gender, nationality, firstName, lastName, streetAddress, zipCode, city, emailAddress, username, phoneNumber)
         
 
-    def printCustomer(self): 
-        return self.firstName + " " + self.lastName + "\nID", self.id
+    
 
 
--- librarian.py
+++ librarian.py
@@ -1,14 +1,6 @@
-import customer
-
-class librarian():
+class librarian(Person):
 
     def __init__(self,name,username):
-        self.name = name
+        super().__init__(id, gender, nationality, firstName, lastName, streetAddress, zipCode, city, emailAddress, username, phoneNumber)
+        self.name = firstName + " " + lastName
         self.username = username
-
-
-
-        
-
-    
-        
